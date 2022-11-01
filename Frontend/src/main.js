2e91d4fbbc3b7e9ba1363a778bacdde46345fde7
mjbmb <mb@boeck.info>
2022-11-01 21:21:22 +0100

    add files

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,11 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 createApp(App).mount('#app')
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
